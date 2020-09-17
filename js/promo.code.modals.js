3c97699cd6a94e9a674b9cf5e824244f4afaa1c9
jeremyasmus <30504512+jeremyasmus@users.noreply.github.com>
2020-09-16 21:01:21 -0700

    Add files via upload

--- js/promo.code.modals.js
+++ js/promo.code.modals.js
@@ -1,6 +1,23 @@
 // PROMO CODE MODAL TRIGGERS
+
+function copyPromoCode() {
+  var copyText = document.getElementById("promoCode");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  var tooltip = document.getElementById("promoCodeTooltip");
+  tooltip.innerHTML =  copyText.value + " copied!";
+}
+
+function outFunc() {
+  var tooltip = document.getElementById("promoCodeTooltip");
+  tooltip.innerHTML = "Copy promo code";
+}
+
 $(document).ready(function() {
   if (window.location.href.indexOf('?offer=pipcorn') > -1) {
     $('#pipcornPromo').modal('show');
+  } else if (window.location.href.indexOf('?offer=platejoy') > -1) { 
+	$('#platejoyPromo').modal('show');
   }
 });
