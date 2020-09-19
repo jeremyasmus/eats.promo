486b357e6adc09704e6b03cde6b0817fea706591
jeremyasmus <30504512+jeremyasmus@users.noreply.github.com>
2020-09-19 13:44:09 -0700

    Add files via upload

--- js/promo.code.modals.js
+++ js/promo.code.modals.js
@@ -1,19 +1,11 @@
 // PROMO CODE MODAL TRIGGERS
 
-$(function(){
-  $("#homepagePromoCodeModals").load("../includes/homepage-promo-code-modals.html"); 
-});
-
 function copyPromoCode(elem) {
   var targetElementID = elem.getAttribute("data-target");
   var copyText = document.getElementById(targetElementID);
   copyText.select();
   copyText.setSelectionRange(0, 99999);
   document.execCommand("copy");
-  //var copyText = document.getElementById("promoCode");
-  //copyText.select();
-  //copyText.setSelectionRange(0, 99999);
-  //document.execCommand("copy");
   
   var tooltip = document.getElementById("promoCodeTooltip");
   tooltip.innerHTML =  "Promo copied!";
