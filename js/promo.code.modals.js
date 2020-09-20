ae4243f1e3b64f605be60d6a881379328fe4c568
jeremyasmus <30504512+jeremyasmus@users.noreply.github.com>
2020-09-20 11:16:58 -0700

    Add files via upload

--- js/promo.code.modals.js
+++ js/promo.code.modals.js
@@ -1,12 +1,41 @@
 // PROMO CODE MODAL TRIGGERS
 
+(function () {
+
+  myHTMLInclude();
+
+  function myHTMLInclude() {
+    var z, i, a, file, xhttp;
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+      if (z[i].getAttribute("w3-include-html")) {
+        a = z[i].cloneNode(false);
+        file = z[i].getAttribute("w3-include-html");
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+          if (xhttp.readyState == 4 && xhttp.status == 200) {
+            a.removeAttribute("w3-include-html");
+            a.innerHTML = xhttp.responseText;
+            z[i].parentNode.replaceChild(a, z[i]);
+            myHTMLInclude();
+          }
+        }
+        xhttp.open("GET", file, true);
+        xhttp.send();
+        return;
+      }
+    }
+  }
+
+})();
+
 function copyPromoCode(elem) {
   var targetElementID = elem.getAttribute("data-target");
   var copyText = document.getElementById(targetElementID);
   copyText.select();
   copyText.setSelectionRange(0, 99999);
   document.execCommand("copy");
-  
+
   var tooltip = document.getElementById("promoCodeTooltip");
   tooltip.innerHTML = "Promo copied!";
 }
@@ -16,14 +45,14 @@ function outFunc() {
   tooltip.innerHTML = "Copy promo code";
 }
 
-$(document).ready(function() {
-  if (window.location.href.indexOf('?offer=splendid-spoon') > -1) { 
-	$('#splendidSpoonPromo').modal('show');
-  } else if (window.location.href.indexOf('?offer=purple-carrot') > -1) { 
-	$('#purpleCarrotPromo').modal('show');
-  } else if (window.location.href.indexOf('?offer=freshly') > -1) { 
-	$('#freshlyPromo').modal('show');
-  } else if (window.location.href.indexOf('?offer=gobble') > -1) { 
-	$('#gobblePromo').modal('show');
+$(document).ready(function () {
+  if (window.location.href.indexOf('?offer=splendid-spoon') > -1) {
+    $('#splendidSpoonPromo').modal('show');
+  } else if (window.location.href.indexOf('?offer=purple-carrot') > -1) {
+    $('#purpleCarrotPromo').modal('show');
+  } else if (window.location.href.indexOf('?offer=freshly') > -1) {
+    $('#freshlyPromo').modal('show');
+  } else if (window.location.href.indexOf('?offer=gobble') > -1) {
+    $('#gobblePromo').modal('show');
   }
 });
