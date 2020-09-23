f998cea9d633923634108fa6b3addb781a945518
jeremyasmus <30504512+jeremyasmus@users.noreply.github.com>
2020-09-23 10:51:38 -0700

    Add files via upload

--- js/includes.js
+++ js/includes.js
@@ -3,7 +3,7 @@
 $(function(){
   var includes = $('[data-include]');
   jQuery.each(includes, function(){
-    var file = 'includes/' + $(this).data('include') + '.html';
+    var file = '/includes/' + $(this).data('include') + '.html';
     $(this).load(file);
   });
 });
